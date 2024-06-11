d398019bda7f1c73e5804fcf5e9e41cb334af878
Jayesh Khairnar <jayeshkhairnar6@gmail.com>
2024-06-11 20:24:10 +0530

    adding GET() CSS HTML

---
+++ WebApi_emprepositorycrud/Program.cs
@@ -0,0 +1,105 @@
+
+using Microsoft.EntityFrameworkCore;
+using WebApi_emprepositorycrud.Repository;
+using WebApi_emprepositorycrud.Service;
+using Microsoft.AspNetCore.Cors;
+
+namespace WebApi_emprepositorycrud
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddTransient<IEmployeeservice, SqlEmployeeservice>();
+            builder.Services.AddDbContext<Appdbcontext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDBConnection")), ServiceLifetime.Transient);
+
+            // Add CORS services
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowLocalhost",
+                    builder =>
+                    {
+                        builder.WithOrigins("http://127.0.0.1:5500")
+                               .AllowAnyHeader()
+                               .AllowAnyMethod();
+                    });
+            });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            // Enable CORS
+            app.UseCors("AllowLocalhost");
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
+
+
+/*
+using Microsoft.EntityFrameworkCore;
+using WebApi_emprepositorycrud.Repository;
+using WebApi_emprepositorycrud.Service;
+
+namespace WebApi_emprepositorycrud
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddTransient<IEmployeeservice, SqlEmployeeservice>();
+            builder.Services.AddDbContext<Appdbcontext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDBConnection")), ServiceLifetime.Transient);
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+           
+
+            app.Run();
+        }
+    }
+}
+*/
---
+++ WebApi_emprepositorycrud/Repository/Appdbcontext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_emprepositorycrud.Models;
+
+namespace WebApi_emprepositorycrud.Repository
+{
+    public class Appdbcontext:DbContext
+    {
+
+        public Appdbcontext(DbContextOptions<Appdbcontext> options)
+                 : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectModels\REST;Initial Catalog=DemoData;Integrated Security=True");
+            }
+        }
+        //Data Source=(localdb)\ProjectModels;Initial Catalog=DemoData;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    
+}
